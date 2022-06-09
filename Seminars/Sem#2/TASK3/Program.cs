275f58878bddcf544bbd0b91c0049f5a2dd3f2c5
kuznetsovmatweyushka <kuznetsovmatweyushka@yandex.ru>
2022-06-09 15:07:14 +0300

    S2T3

---
+++ Seminars/Sem#2/TASK3/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая будет принимать на
+вход два числа и выводить, является ли второе число
+кратным первому. Если число 2 не кратно числу 1, то
+программа выводит остаток от деление. */
+Console.WriteLine("Введите число №1: ");
+int numbA =int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число №2: ");
+int numbB =int.Parse(Console.ReadLine());
+int remain = numbB % numbA;
+if (remain == 0)
+{
+    Console.Write("Число №2 кратно числу №1.");
+}
+else
+{
+    Console.Write("Число №2 некратно числу №1, остаток равен: "+remain);
+}
