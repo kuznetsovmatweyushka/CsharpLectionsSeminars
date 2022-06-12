07bda8a8199fc0b178ca798d5ecf38173dfe8e26
kuznetsovmatweyushka <kuznetsovmatweyushka@yandex.ru>
2022-06-12 20:10:16 +0300

    up

--- Seminars/Sem#3/TASK3/Program.cs
+++ Seminars/Sem#3/TASK3/Program.cs
@@ -2,13 +2,13 @@
 принимает на вход координаты двух точек и 
 находит расстояние между ними в 2D 
 пространстве. */
-Console.WriteLine("Введите Xa: ");
+Console.Write("Введите Xa: ");
 double Xa = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите Ya: ");
+Console.Write("Введите Ya: ");
 double Ya = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите Xb: ");
+Console.Write("Введите Xb: ");
 double Xb = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите Yb: ");
+Console.Write("Введите Yb: ");
 double Yb = double.Parse(Console.ReadLine());
-double distanse = Math.Sqrt(Math.Pow(Xb - Xa,2) + Math.Pow(Yb - Ya,2));
-Console.WriteLine("Расстояние :" + distanse);
+double distanse = Math.Sqrt(Math.Pow(Xb - Xa, 2) + Math.Pow(Yb - Ya, 2));
+Console.WriteLine("Расстояние:" + distanse);
