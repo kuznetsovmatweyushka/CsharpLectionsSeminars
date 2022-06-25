509c5262e7aebe2f04bfdab287fae01ee133139d
kuznetsovmatweyushka <kuznetsovmatweyushka@yandex.ru>
2022-06-25 16:39:14 +0300

    up

--- Seminars/Homework(Sem#6)/Program.cs
+++ Seminars/Homework(Sem#6)/Program.cs
@@ -23,21 +23,21 @@ Console.WriteLine($"Количество чисел больше 0: " + more0); 
 пересечения двух прямых, заданных уравнениями y = k1 *
 x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
 пользователем. */
-/* System.Console.WriteLine("Введите b1: ");
+/* Console.WriteLine("Введите b1: ");
 double b1 = double.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите k1: ");
+Console.WriteLine("Введите k1: ");
 double k1 = double.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите b2: ");
+Console.WriteLine("Введите b2: ");
 double b2 = double.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите k2: ");
+Console.WriteLine("Введите k2: ");
 double k2 = double.Parse(Console.ReadLine());
 if (k1 == k2)
 {
-    System.Console.WriteLine("Прямые совпадают!");
+    Console.WriteLine("Прямые совпадают!");
 }
 else if (b1 == b2)
 {
-    System.Console.WriteLine("Прямые параллельны!");
+    Console.WriteLine("Прямые параллельны!");
 }
 else
 {
@@ -45,12 +45,9 @@ else
     {
 
         double x = (b2 - b1) / (k1 - k2);
-        double y = k2 * x + b2;
         return x;
-        return y;
     }
     double x = Math.Round(Point(b1, k1, b2, k2),2);
-    double y = Math.Round(Point(b1, k1, b2, k2),2);
-    Console.WriteLine($"({x} ; {y})");
+    Console.WriteLine($"({x} ; {Math.Round((k2*x+b2),2)})");
 } */
 
